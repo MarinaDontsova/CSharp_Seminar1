cac0a65e98443aa24df7044e1a6fa0a65092053c
Марина Донцова <dontsovam@mail.ru>
2022-09-19 00:29:24 +0300

    Решила задачу №8 - вывод всех четных чисел от 1 до N

---
+++ hw_Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int A = 2;
+
+while(A <= N)
+{
+    Console.Write(A + " ");
+    A = A + 2;
+}
+Console.WriteLine(" ");
